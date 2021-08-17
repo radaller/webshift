d85536201db6fa04e39742d560792fc133e55928
osvarychevskyi <a.ekspo@gmail.com>
2021-08-17 17:06:27 +0200

    - moved script tag with initial data from render to document

--- src/server/render.js
+++ src/server/render.js
@@ -9,11 +9,10 @@ import logger from '@logger';
 import Document from '@document';
 
 export default ({ clientStats }) => {
-    return async (req, res, next) => {
+    return async (req, res) => {
 
         const routerContext = {};
-        const chunkExtractor = new ChunkExtractor({ stats: clientStats, namespace: FRAGMENT_ID, publicPath: process.env.PUBLIC_PATH});
-
+        const chunkExtractor = new ChunkExtractor({ stats: clientStats, namespace: FRAGMENT_ID, publicPath: process.env.PUBLIC_PATH });
         const ServerApp = () =>
             <ChunkExtractorManager extractor={ chunkExtractor }>
                 <StaticRouter context={ routerContext } location={ req.url } basename={ process.env.BASE_PATH }>
@@ -39,26 +38,17 @@ export default ({ clientStats }) => {
                 <ServerApp />
             </DataProvider>;
 
-        const Scripts = () => [
-            ...chunkExtractor.getScriptElements(),
-            <script
-                id={ `${ FRAGMENT_ID }__INITIAL_DATA` }
-                type={ "application/json" }
-                dangerouslySetInnerHTML={ { __html: JSON.stringify(data) } }
-            />,
-        ];
-
         logger.verbose({ message: '[ChunkExtractor]', meta: { chunks: chunkExtractor.chunks } });
 
         res.send('<!DOCTYPE html>' + renderToString(
             <Document
                 Headers={ App.Headers }
                 App={ ServerAppWithData }
-                Scripts={ Scripts }
+                scripts={ [ ...chunkExtractor.getScriptElements() ] }
                 esi_enabled={ req.header('esi') === 'true' }
                 fragment_id={ FRAGMENT_ID }
+                initialData={ data }
             />
         ));
-        next();
     };
 };
